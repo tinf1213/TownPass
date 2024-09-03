7dbf43e09b8ba5cea05625dcf3b11cb85edcf565
sunset513 <amy39899009@gmail.com>
2024-09-04 01:43:21 +0800

    fix: fix merge

--- web/frontend/app.vue
+++ web/frontend/app.vue
@@ -1,6 +1,13 @@
+<!-- 全域樣式 -->
+<style>
+/* 頁面過渡動畫 */
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.1s;
+}
+</style>
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage/>
   </div>
 </template>
