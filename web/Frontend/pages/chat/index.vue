2431183a63c1946db6d05a41811b7c24e5e252f5
sunset513 <amy39899009@gmail.com>
2024-09-06 22:40:59 +0800

    init: succesfully change pages

---
+++ web/Frontend/pages/chat/index.vue
@@ -0,0 +1,12 @@
+
+<script setup>
+import TextList from '@/components/TextList.vue';
+
+const texts = ['Hello', 'World', 'This', 'Is', 'Vue'];
+</script>
+
+<template>
+  <div>
+    <TextList :texts="texts" />
+  </div>
+</template>
