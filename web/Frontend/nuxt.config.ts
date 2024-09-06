7d7b5dcf3fbe9a583d42b54f8e6cd3f2e8f66960
sunset513 <amy39899009@gmail.com>
2024-09-06 23:14:17 +0800

    succesfully switch pages

--- web/Frontend/nuxt.config.ts
+++ web/Frontend/nuxt.config.ts
@@ -1,9 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   devServer: {
     host: '0.0.0.0', // 监听所有接口，包括 127.0.0.1
     port: 3000
   },
+  runtimeConfig: {
+    public: {
+      OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+      GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+      AZURE_API_KEY: process.env.AZURE_API_KEY,
+      AZURE_ENDPOINT: process.env.AZURE_ENDPOINT
+    }
+  }
 })
